c3c77f5a29ad77beb01ee764ee28dfd9e4faf8ba
sunny77889 <44544813+sunny77889@users.noreply.github.com>
2023-06-11 15:24:09 +0800

    Update data_product_1.py

--- 对比实验/ML/data_product_1.py
+++ 对比实验/ML/data_product_1.py
@@ -8,7 +8,7 @@ pcap_dir='pcap_data'
 out_dir='data_features'
 for filename in os.listdir(pcap_dir):
     x_black = pre.pre_pcap(pcap_dir+'/'+filename, file_name)
-    f = open(pcap_dir+'/'+filename+".csv", 'w+', newline='')
+    f = open(out_dir+'/'+filename+".csv", 'w+', newline='')
     f_csv = csv.writer(f)
     for key in x_black:
         f_csv.writerow(key)
